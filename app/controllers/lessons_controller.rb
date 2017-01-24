c8c59ec5a348e32264ab98841706b5d310500684
Anna Canelas <canelasa2@gmail.com>
2017-01-24 14:14:55 +0000

    Handle form submission

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,4 +6,15 @@ class LessonsController < ApplicationController
   def new
     @lesson = Lesson.new
   end
+
+  def create
+   Lesson.create(lesson_params)
+   redirect_to root_path
+  end
+
+ private
+
+  def lesson_params
+   params.require(:lesson).permit(:name, :description, :date, :time, :location, :address)
+  end
 end
