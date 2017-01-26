93786302984c26cf8d479ca5b323dd1be6308d6b
Anna Canelas <canelasa2@gmail.com>
2017-01-26 13:05:58 +0000

    Modify edit and destroy user permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,5 @@
 class LessonsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @lessons = Lesson.paginate(page: params[:page], per_page: 5)
   end
@@ -19,16 +19,28 @@ class LessonsController < ApplicationController
 
   def edit
      @lesson = Lesson.find(params[:id])
+
+     if @lesson.user != current_user
+       return render text: 'Not Allowed', status: :forbidden
+     end
   end
 
   def update
     @lesson = Lesson.find(params[:id])
+    if @lesson.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
     @lesson.update_attributes(lesson_params)
     redirect_to root_path
   end
 
   def destroy
     @lesson = Lesson.find(params[:id])
+    if @lesson.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
     @lesson.destroy
     redirect_to root_path
   end
