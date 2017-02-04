23c1f2d4ded8dad2e728a2bbedf549e111681634
Anna Canelas <canelasa2@gmail.com>
2017-02-04 22:47:42 +0000

    Make New Lesson button visible only to admin

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  # before_action :require_admin, only: [:new, :create, :edit, :update, :destroy]
   def index
     @lessons = Lesson.order("created_at DESC").paginate(page: params[:page], per_page: 5)
   end
@@ -59,4 +60,11 @@ class LessonsController < ApplicationController
   def lesson_params
    params.require(:lesson).permit(:name, :description, :date, :time, :location, :address)
   end
+
+  # def require_admin
+  #   if current_user != current_user && current_user.admin
+  #     flash[:danger] = "Only instructors can create new lessons"
+  #     redirect_to root_path
+  #   end
+  # end
 end
