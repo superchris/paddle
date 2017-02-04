cf3368ce6614f71b1c7d2369301e0181c34e1323
Anna Canelas <canelasa2@gmail.com>
2017-02-04 01:43:14 +0000

    Make contact me button work

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,9 +1,7 @@
 class InstructorsController < ApplicationController
   def index
-    # @instructors = Instructor.all
   end
 
   def show
-    @instructor = Instructor.find(params[:id])
   end
 end
